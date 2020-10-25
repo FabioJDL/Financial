155a0e6c034d14a81869c00307840c6e40078e74
Fábio Dias <fabiodias.eng@gmail.com>
2020-10-25 19:50:13 -0300

    Swagger

---
+++ Models/ContaPessoaFisica.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+using Financial.Models.Enum;
+
+namespace Financial.Models
+{
+    public class ContaPessoaFisica : Conta
+    {
+        public string NomeCompleto { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public string EstadoCivil { get; set; }
+        public Documento Documento { get; set; }
+        public Genero Genero { get; set; }
+    }
+}
