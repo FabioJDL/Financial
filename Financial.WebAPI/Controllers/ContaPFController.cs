c22b4709f10a4a01aeaf6ab718dc6ce405e01383
Fábio Dias <fabiodias.eng@gmail.com>
2020-10-26 09:20:40 -0300

    Swagger

---
+++ Financial.WebAPI/Controllers/ContaPFController.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Financial.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContaPFController : ControllerBase
+    {
+        private static readonly string[] TipoConta = new[]
+        {
+            "Poupança", "Conta Corrente", "Conta Salário", "Depósito Judicial"
+        };
+
+        private static readonly string[] Nome = new[]
+        {
+            "João", "Maria", "Cleuza", "José"
+        };
+
+        private readonly ILogger<ContaPFController> _logger;
+
+        public ContaPFController(ILogger<ContaPFController> logger)
+        {
+            _logger = logger;
+        }
+
+        /// <summary>
+        /// Lista de contas pessoa física
+        /// </summary>
+        /// <returns></returns>
+
+        [HttpGet]
+        public IEnumerable<ContaPF> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new ContaPF
+            {
+                Agencia = rng.Next(1111, 9999),
+                Conta = rng.Next(11111, 99999),
+                TipoConta = TipoConta[rng.Next(TipoConta.Length)],
+                NomeCompleto = Nome[rng.Next(Nome.Length)],
+            })
+            .ToArray();
+        }
+
+
+        // GET: api/<ContaPFController>
+        //[HttpGet]
+        //public IEnumerable<string> Get()
+        //{
+        //    return new string[] { "value1", "value2" };
+        //}
+
+        // GET api/<ContaPFController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<ContaPFController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<ContaPFController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<ContaPFController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
