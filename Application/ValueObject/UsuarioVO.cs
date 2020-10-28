7bcbf49c984c941ba6ecf0d5418841c5e910ca20
Fábio Dias <fabiodias.eng@gmail.com>
2020-10-28 08:41:06 -0300

    Inclusão da pasta VO

---
+++ Application/ValueObject/UsuarioVO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Financial.Application.ValueObject
+{
+    public class UsuarioVO
+    {
+        public int Id { get; set; }
+        public string NomeCompleto { get; set; }
+        public string CPF { get; set; }
+        public string TelefoneFixo { get; set; }
+        public string TelefoneCelular { get; set; }
+    }
+}
